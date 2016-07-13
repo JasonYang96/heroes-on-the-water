282304fb0162c358ba0b61a67aedbf6d0b8538a7
GARG <rg9658@att.com>
2016-07-13 09:23:41 -0700

    broken: user auth

--- server.js
+++ server.js
@@ -5,20 +5,40 @@ var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var passport = require('passport');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+
 
 // database configuration
 var database = require('./config/database');
 mongoose.connect(database.url);
 
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+app.set('view engine', 'ejs');
 
-// load the routes
-require('./app/eventRoutes.js')(app);
+//set up passport
+require('./config/passport')(passport); // pass passport for configuration
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+//load-routes
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// // load the routes
+// require('./app/eventRoutes.js')(app);
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
