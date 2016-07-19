1557fd7fffeddfd36db4df1c4c11654e4097d723
Yang, Jason (jy092m) <jy092m@us.att.com>
2016-07-19 14:12:21 -0700

    linked manager views with profile

--- app/routes/managerRoutes.js
+++ app/routes/managerRoutes.js
@@ -19,9 +19,9 @@ module.exports = function(app) {
     });
 
     // ADMIN API
-    // admin view for a specific region
-    app.get('/:region_name', function(req, res) {
-
+    //admin view for a specific region
+    app.get('/api/:region_name', function(req, res) {
+        console.log("testing");
         Model.region.findOne({"name": req.params.region_name}, function(err, region) {
             if (err)
                 res.send(err);
@@ -36,7 +36,7 @@ module.exports = function(app) {
     });
 
     // admin view for specific chapter
-    app.get('/:region_name/:chapter_name', function(req, res) {
+    app.get('/api/:region_name/:chapter_name', function(req, res) {
         Model.region.aggregate([
             { $match: {"name": req.params.region_name}},
             { $unwind: "$chapters"},
@@ -55,13 +55,72 @@ module.exports = function(app) {
     });
 
     // admin view for specific event
-    app.get('/:region_name/:chapter_name/:event_name', function(req, res) {
+    app.get('/api/:region_name/:chapter_name/:event_name', function(req, res) {
         Model.region.aggregate([
             { $match: {"name": req.params.region_name}},
             { $unwind: "$chapters"},
             { $match: {"chapters.name": req.params.chapter_name}},
             { $unwind: "$chapters.events"},
             { $match: {"chapters.events.name" : req.params.event_name}}
+        ]).exec(function(err, region) {
+            if (err)
+                res.send(err);
+
+            res.render('./ejs/manager/event.ejs', {region: region[0]}, function(err, html) {
+                if (err) {
+                    res.send(err);
+                }
+                res.send(html);
+            });
+        });
+    });
+
+    // Internal API
+    // internal view for a specific region
+    app.get('/:region_id', function(req, res) {
+        console.log(req.params.region_id);
+        Model.region.findOne({"_id": req.params.region_id}, function(err, region) {
+            if (err)
+                res.send(err);
+
+            res.render('./ejs/manager/region.ejs', {region: region}, function(err, html) {
+                if (err) {
+                    res.send(err);
+                }
+                res.send(html);
+            });
+        });
+    });
+
+    // internal view for specific chapter
+    app.get('/:region_id/:chapter_id', function(req, res) {
+        console.log("testing");
+        Model.region.aggregate([
+            { $match: {"_id": req.params.region_id}},
+            { $unwind: "$chapters"},
+            { $match: {"chapters._id": req.params.chapter_id}},
+        ]).exec(function(err, region) {
+            if (err)
+                res.send(err);
+
+            res.render('./ejs/manager/chapter.ejs', {region: region[0]}, function(err, html) {
+                if (err) {
+                    res.send(err);
+                }
+                res.send(html);
+            });
+        });
+    });
+
+    // internal view for specific event
+    app.get(':region_id/:chapter_id/:event_id', function(req, res) {
+        console.log("test");
+        Model.region.aggregate([
+            { $match: {"_id": req.params.region_id}},
+            { $unwind: "$chapters"},
+            { $match: {"chapters._id": req.params.chapter_id}},
+            { $unwind: "$chapters.events"},
+            { $match: {"chapters.events._id" : req.params.event_id}}
         ]).exec(function(err, region) {
             if (err)
                 res.send(err);
