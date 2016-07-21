642f9f0edd11a4732d5d6f426b924ea510276ab6
GARG <rg9658@att.com>
2016-07-21 13:29:43 -0700

    tons of profile stuff

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -52,8 +52,14 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('./ejs/main/profile.ejs', {
+        res.render('./ejs/main/profileRak.ejs', {
             user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    app.get ('/profileOld', isLoggedIn, function (req, res) {
+        res.render('./ejs/main/profile.ejs', {
+            user: req.user
         });
     });
 
