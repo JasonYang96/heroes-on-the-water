575e3ab4f08aa9e3edc46381455411a8d888bfaa
RODRIGUEZ <cr056n@att.com>
2016-07-21 10:14:54 -0700

    Added donate subforms to db.

--- app/routes/donorRoutes.js
+++ app/routes/donorRoutes.js
@@ -6,28 +6,82 @@ var Donor = mongoose.model('donor', {
     phone: String,
     email: String,
     donation: Number,
-    description: Number
+    description: Number,
+
+    //sub-forms fields are all optional
+    //Specific chapter
+    chapter: {
+        type: String,
+        required: false
+    },
+    //Memory of loved one
+    loved_one: {
+        type: String,
+        required: false
+    },
+    //Shared letter fields
+    letter_fName: {
+        type: String,
+        required: false
+    },
+    letter_lName: {
+        type: String,
+        required: false
+    },
+    letter_street: {
+        type: String,
+        required: false
+    },
+    letter_city: {          //<--also shared for product donation
+        type: String,
+        required: false
+    },
+    letter_state: {         //<--also shared for product donation
+        type: String,
+        required: false
+    },
+    letter_zip: {           //<--also shared for product donation
+        type: String,
+        required: false
+    },
+    letter_country: {
+        type: String,
+        required: false
+    },
+    //Product donation fields
+    product_what: {
+        type: String,
+        required: false
+    },
+    product_you: {
+        type: String,
+        required: false
+    },
+    product_hear: {
+        type: String,
+        required: false
+    }
 });
 
 // -------------------------DONOR API -------------------------------------//
-module.exports = function(app) {
-    var getDonors = function(res) {
-        Donor.find(function(err, donors) {
+module.exports = function (app) {
+    var getDonors = function (res) {
+        Donor.find(function (err, donors) {
             if (err)
                 res.send(err)
 
             res.json(donors);
         });
     };
-    
+
     // get all donors
-    app.get('/api/donors', function(req, res) {
+    app.get('/api/donors', function (req, res) {
         getDonors(res);
     });
 
     // create a new donor
-    app.post('/api/donors/', function(req, res) {
-        console.log("donor Server -- ")
+    app.post('/api/donors/', function (req, res) {
+        console.log("== donorRoutes == ")
         console.log(req);
         Donor.create({
             fName: req.body.fName,
@@ -35,18 +89,32 @@ module.exports = function(app) {
             phone: req.body.phone,
             email: req.body.email,
             donation: req.body.donation,
-            description: req.body.description
-        
-        }, function(err, regions) {
-            if (err)
-                res.send(err);
+            description: req.body.description,
 
-            getDonors(res);
-        })
+            chapter: req.body.chapter,
+            loved_one: req.body.loved_one,
+            letter_fName: req.body.letter_fName,
+            letter_lName: req.body.letter_lName,
+            letter_street: req.body.letter_street,
+            letter_city: req.body.letter_city,
+            letter_state: req.body.letter_state,
+            letter_zip: req.body.letter_zip,
+            letter_country: req.body.letter_country,
+            
+            product_what: req.body.product_what,
+            product_you: req.body.product_you,
+            product_hear: req.body.product_hear
+
+            }, function(err, regions) {
+                if (err)
+                    res.send(err);
+
+                getDonors(res);
+            })
     });
 
-    app.get('/donate', function(req, res) {
-        res.render('./ejs/donate/donateRak.ejs', function(err, html) {
+    app.get('/donate', function (req, res) {
+        res.render('./ejs/donate/donate.ejs', function (err, html) {
             if (err) {
                 res.send(err);
             }
@@ -54,8 +122,8 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/donors', function(req, res) {
-        res.render('./ejs/donate/donors.ejs', function(err, html) {
+    app.get('/donors', function (req, res) {
+        res.render('./ejs/donate/donors.ejs', function (err, html) {
             if (err) {
                 res.send(err);
             }
