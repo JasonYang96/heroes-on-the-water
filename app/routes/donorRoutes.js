b23db99fbf9eb69f33b443501b7132cb041113a5
RODRIGUEZ <cr056n@att.com>
2016-07-21 16:17:56 -0700

    Added admin_donors.ejs for admin donors page (displays all donors info). Has collapsable table cols. Little arrows don't function.

--- app/routes/donorRoutes.js
+++ app/routes/donorRoutes.js
@@ -133,3 +133,5 @@ module.exports = function (app) {
 
 
 }
+
+module.exports.donor = Donor;
--- app/routes/managerRoutes.js
+++ app/routes/managerRoutes.js
@@ -1,5 +1,6 @@
 // list of manager views for admin, regions, chapters, and events
 var Model = require('../models/event');
+var donorModel = require('./donorRoutes.js');
 
 module.exports = function(app) {
     // admin view for everything
@@ -13,6 +14,23 @@ module.exports = function(app) {
                 if (err) {
                     res.send(err);
                 }
+                res.send(html);
+            })
+        });
+    });
+
+    // admin view of donors
+    app.get('/admin/donors', function(req, res) {
+        donorModel.donor.find(function(err, donors) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.render('./ejs/manager/admin_donors.ejs', {donors: donors}, function(err, html) {
+                if (err) {
+                    res.send(err);
+                }
+
                 res.send(html);
             })
         });
